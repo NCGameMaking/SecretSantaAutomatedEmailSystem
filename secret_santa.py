e55936c91ab6c4fd9be5de31b99c12fc7d717097
NCGameMaking <neil726345@gmail.com>
2026-07-07 13:23:57 +1000

    Just debugged it and edited the script so it actually works.

--- secret_santa.py
+++ secret_santa.py
@@ -1,16 +1,19 @@
-from dotenv import load_dotenv
 import os
 import random
 import smtplib
 import ssl
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
 def send_email(sender, reciever, recipient):
-    password = os.environ['password']
+    password = os.getenv('password')
+
     body_msg = f'''\
 From: {sender}
-Subject: Your Secret Santa Present
+To:{reciever}
+Subject: Your Secret Santa Assignment
 
 Hello! Your secret santa is: {recipient}!
 Remember to spend $10-$20 on your gift, but don't stress about it being the perfect gift!
@@ -18,23 +21,39 @@ Remember to spend $10-$20 on your gift, but don't stress about it being the perf
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
         server.login(sender, password)
-        server.sendmail(sender, reciever, body_msg)
+        server.sendmail(sender, reciever, body_msg.encode('utf-8'))
 
-names_list = ['Mum', 'alt neil', 'main neil']
-names_and_emails = [
-  ['mum', 'ddc2010@gmail.com'],
-  ['2020Neilrishi', '2020neilrishi@gmail.com'],
-  ['Neil', 'neil726345@gmail.com']
+names_list = ['Subject A', 'Subject B', 'Subject C', 'Subject D']
+participants = [
+  ['Subject A', 'subjectA@gmail.com'],
+  ['Subject B', 'subjectB@gmail.com'],
+  ['Subject C', 'subjectC@gmail.com'],
+  ['Subject D', 'subjectD@eq.edu.au']
   ]
 
 if len(names_list) <= 1:
     print('not enough people to start secret santa!')
     quit()
 
-first_name = names_and_emails[0][0]
+targets = participants.copy()
 
-while len(names_list) >=2:
-    send_email('<neil726345@gmail.com>', names_and_emails[0][1], names_and_emails[1][0])
-    names_and_emails.pop(0)
-    random.shuffle(names_and_emails)
-send_email('neil726345@gmail.com', names_and_emails[0][1], first_name)
+while True:
+    random.shuffle(targets)
+    matched_self = False
+    for i in range(len(participants)):
+        if participants[i][0] == targets[i][0]:
+            matched_self = True
+            break
+    if not matched_self:
+        break
+
+sender_email = 'neil726345@gmail.com'
+
+for i in range(len(participants)):
+    giver_name = participants[i][0]
+    giver_email = participants[i][1]
+    reciever_name = targets[i][0]
+
+    print(f"Sending email to {giver_name}...")
+    send_email(sender_email, giver_email, reciever_name)
+print("All secret santas emails sent successfully")
